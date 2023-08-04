fa0bc552fe0c823ef40fa1d9efa764e5ba134726
Dmytro Popov <thedmitryp@ukr.net>
2023-08-05 00:47:48 +0100

    `test_utils@0.0.2`
    `repeat` and `repeatWithArgument` functions now throw a `StateError` when the `times` argument is less than 1

--- packages/test_utils/CHANGELOG.md
+++ packages/test_utils/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- `repeat` and `repeatWithArgument` now throw a StateError when the `times` argument is less than 1. 
+
 ## 0.0.1
 
 - Initial version.
--- packages/test_utils/lib/src/loops.dart
+++ packages/test_utils/lib/src/loops.dart
@@ -2,7 +2,7 @@ import 'typedefs.dart';
 
 /// Repeats the function [fn] [times] times.
 void repeat(VoidCallback fn, {required int times}) {
-  assert(times > 0);
+  _checkRepeatTimes(times);
 
   for (var i = 0; i < times; i++) {
     fn();
@@ -15,10 +15,14 @@ void repeatWithArgument<T>(
   required int times,
   required ArgumentSupplier<T> argumentSupplier,
 }) {
-  assert(times > 0);
+  _checkRepeatTimes(times);
 
   for (var run = 0; run < times; run++) {
     final argument = argumentSupplier(run);
     fn(argument);
   }
 }
+
+/// Throws a [StateError] if [times] is less than 1.
+void _checkRepeatTimes(int times) =>
+    times > 0 ? null : throw StateError('`times` must be greater than or equal to 1');
